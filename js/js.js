3d889c83990d83ae4058f9148111185a060e9bc3
LilBear1811 <juandavidfernandezpardo@gmail.com>
2024-02-22 15:33:40 -0500

    Actualización de archivo

---
+++ js/js.js
@@ -0,0 +1,8 @@
+//titulo y parrafos
+
+document.getElementById("nombre").innerText='Juan David'
+document.getElementById("apellido").innerText='Fernandez Pardo'
+document.getElementById("telefono").innerText='3208762427'
+document.getElementById("correo").innerText='juandavidfernandezpardogmail.com'
+document.getElementById("edad").innerText='18'
+document.getElementById("hobby").innerText='Mi hobby es programar y jugar videojuegos'
